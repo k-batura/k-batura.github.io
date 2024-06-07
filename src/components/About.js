64567e96fef390020627cf350034c5534285f3ca
Kash Patel <68030255+Kashyap5551@users.noreply.github.com>
2024-06-07 03:51:38 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,28 @@
 // src/components/About.js
 
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 export default function About() {
+
+  const experiences = [
+  {
+    title: "Software Engineering Intern",
+    company_name: "Moody's Analytics",
+    icon: moodys,
+    iconBg: "#1B95E1",
+    date: "May 2022 - August 2022",
+    points: [
+      "Developing and maintaining web applications using Java Spring and other related technologies.",
+      "Collaborating with cross-functional teams including designers, Business Analysts, and other developers to create high-quality products.",
+      "Implementing scalable REST APIs using Spring framework ensuring robust architechture.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  }
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
@@ -35,6 +55,68 @@ export default function About() {
             alt="hero"
             src="./coding.svg"
           />
+        </div>
+      </div>
+
+
+               <div className="py-16">
+        <h3 className="subhead-text">Work Experience.</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            I've worked within all sorts of organizations, from start-ups to
+            enterprises. Leveling up my skills and teaming up with exceptional
+            people. Here's the rundown:
+          </p>
+        </div>
+
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                iconStyle={{ background: experience.iconBg }}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
+                    <img
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className="w-[60%] h-[60%] object-contain"
+                    />
+                  </div>
+                }
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: "none",
+                }}
+              >
+                <div>
+                  <h3 className="text-black text-xl font-poppins font-semibold">
+                    {experience.title}
+                  </h3>
+                  <p
+                    className="text-black-500 font-medium text-base"
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className="text-black-900 font-normal pl-1 text-sm"
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
         </div>
       </div>
     </section>
