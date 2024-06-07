2ed61dd77f8dde402450d1ad0100bfd86d9277be
unknown <kbatura@my.yorku.ca>
2024-06-07 02:01:03 -0400

    adding website source

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Exercise Tracker App",
+    subtitle: "MERN Stack",
+    description:
+      "A fullstack exercise tracker app with CRUD (Create, Read, Update and Delete) operation, built using MongoDB, Express, React, Node.js. Database hosted on MongoDB Atlas and Google Cloud Platform.",
+    image: "./project-1.gif",
+    link: "https://github.com/k-batura/MERNstack-exercise-tracker",
+  },
+  {
+    title: "Exercise Tracker App",
+    subtitle: "React, MaterialUI, Netlify and various APIs",
+    description:
+      "A travel companion app that has many features such as searching for places, fetching restaurants, hotels and attractions based on location from specialized Rapid APIs, data filtering and more.",
+    image: "./project-2.gif",
+    link: "https://github.com/k-batura/travel_advisor",
+  },
+  {
+    title: "This Website",
+    subtitle: "React, Tailwind CSS and Netlify",
+    description:
+      "My portfolio website created using advanced React, Tailwind CSS and Netlify for deployment and forms.",
+    image: "./project-3.gif",
+    link: "https://k-batura.netlify.app/",
+  },
+  {
+    title: "Work in progress...",
+    subtitle: "[Info Pending]",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "http://example.com/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node.js",
+  "Python",
+  "C/C++",
+  "Java",
+  "Unix Shell & Bash",
+  "SQL",
+  "HTML",
+  "CSS",
+  "GraphQL",
+  "Material UI",
+];
